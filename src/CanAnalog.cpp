b17fbdaed7e022e755ab2b49b7d74c05fec3445c
Spencer Perkins <hello@spencerperkins.me>
2019-02-23 11:55:39 -0800

    Updated data size for analog

--- src/CanAnalog.cpp
+++ src/CanAnalog.cpp
@@ -4,7 +4,7 @@ CanAnalog::CanAnalog(PinName pin, unsigned canID_) : CanItem(canID_), analog(pin
 
 void CanAnalog::poll() {
     float dataAsFloat = analog.read(); // Between 0 and 1
-    char data[10]; 
+    char data[12]; 
     sprintf(data, "%.3f", dataAsFloat);
     sendMessage(data);
 }
