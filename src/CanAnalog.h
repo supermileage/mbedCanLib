f9960a501c2cdb769d7439f9201690c8d36e4f45
Spencer Perkins <hello@spencerperkins.me>
2019-02-23 12:51:55 -0800

    removed globals.h

--- src/CanAnalog.h
+++ src/CanAnalog.h
@@ -4,7 +4,7 @@
 #ifndef CANANALOG_H
 #define CANANALOG_H 
 
-class CanAnalog : private CanItem {
+class CanAnalog : public CanItem {
     public:
         CanAnalog(PinName pin, unsigned canID_);
         void poll();
