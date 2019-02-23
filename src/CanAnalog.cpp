392120aad868ebd5666a998828d774d708b7f6ba
Spencer Perkins <hello@spencerperkins.me>
2019-02-23 12:46:17 -0800

    Added read function to canAnalog

--- src/CanAnalog.cpp
+++ src/CanAnalog.cpp
@@ -3,9 +3,13 @@
 CanAnalog::CanAnalog(PinName pin, unsigned canID_) : CanItem(canID_), analog(pin) {}
 
 void CanAnalog::poll() {
-    float dataAsFloat = analog.read(); // Between 0 and 1
+    float dataAsFloat = read(); // Between 0 and 1
     char data[12]; 
     sprintf(data, "%.3f", dataAsFloat);
     sendMessage(data);
+}
+
+float CanAnalog::read() {
+    return analog.read(); // Between 0 and 1
 }
 
